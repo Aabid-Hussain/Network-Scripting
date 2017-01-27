e11195c7cb8dc79096859c0496d4c663fcb7ccb9
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-01-27 03:19:55 -0800

    This is modified program of telnet

--- telnet.py
+++ telnet.py
@@ -1,19 +1,27 @@
 import telnetlib
 import os
 import time
+import getpass
 
-Device_Ip = "172.26.106.50"
+Device_Ip = raw_input("Enter Remote Host IP: ")
 
 Router_1 = telnetlib.Telnet(Device_Ip)
 
-Router_1.read_until("Password:",3)
-Router_1.write("Cisco"+"\n")
+#use getpass.getpass() to prompt for password
+
+Password_Logging = getpass.getpass(prompt='Enter Logging Password: ')
+
+if Password_Logging:
+    Router_1.read_until("Password:",3)
+    Router_1.write(Password_Logging + "\n")
 
 Router_1.read_until("R1>",2)
 Router_1.write("enable"+"\n")
 
-Router_1.read_until("Password:",4)
-Router_1.write("Cisco"+"\n")
+Password_Enable = getpass.getpass(prompt='Enter Enable Password: ')
+if Password_Enable:
+    Router_1.read_until("Password:",4)
+    Router_1.write(Password_Enable + "\n")
 
 Router_1.read_until("R1#",5)
 Router_1.write("show ip interface brief"+ "\r\n")
