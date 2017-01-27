30af48f0a37ea67d4b17baa2f93afdf45faa7bb2
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-01-27 03:33:05 -0800

    Fixed indentation of program in telnet

--- telnet.py
+++ telnet.py
@@ -14,25 +14,22 @@ Password_Logging = getpass.getpass(prompt='Enter Logging Password: ')
 if Password_Logging:
     Router_1.read_until("Password:",3)
     Router_1.write(Password_Logging + "\n")
-
-Router_1.read_until("R1>",2)
-Router_1.write("enable"+"\n")
+    Router_1.read_until("R1>",2)
+    Router_1.write("enable"+"\n")
 
 Password_Enable = getpass.getpass(prompt='Enter Enable Password: ')
 if Password_Enable:
     Router_1.read_until("Password:",4)
     Router_1.write(Password_Enable + "\n")
+    Router_1.read_until("R1#",5)
+    Router_1.write("show ip interface brief"+ "\r\n")
+    Inter_Brief = Router_1.read_until("R1#",3)
+    Router_1.write("show running-config | section interface"+ "\r\n")
+    Router_1.write("show ip route"+ "\r\n")
+    Run_Conf = Router_1.read_until("R1#",5)
+    print Inter_Brief
+    print Run_Conf
 
-Router_1.read_until("R1#",5)
-Router_1.write("show ip interface brief"+ "\r\n")
-
-Inter_Brief = Router_1.read_until("R1#",3)
-Router_1.write("show running-config | section interface"+ "\r\n")
-Router_1.write("show ip route"+ "\r\n")
-Run_Conf = Router_1.read_until("R1#",5)
-print Inter_Brief
-
-print Run_Conf
 Router_1.close()
 
 
