df91f42763679c2f8013d5cfa610579228e28044
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-02-03 02:37:29 -0800

    Code to find IPV4 address pattern, otherway of doing it

---
+++ IpRegEx_3.py
@@ -0,0 +1,11 @@
+# This is another method of find IPv4 address pattern
+
+import re
+
+randStr = "adfd sdf 12 25.25 5.255.255 55.5 192.169.1.2 1.1.1.1 adf 2.0 999.999.999.999"
+RegEx = r"\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
+
+matches = re.findall(RegEx,randStr)
+
+for lines in matches:
+    print lines
