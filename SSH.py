f96edfec2bc96857be04b77ccd83d842918d3883
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-03-30 09:52:13 -0700

    modification has been done

--- SSH.py
+++ SSH.py
@@ -1,6 +1,8 @@
 import paramiko
 
+# To open SSH client
 ssh = paramiko.SSHClient()
+#adding policies to connection, if it is not defined code will through error while connecting device.
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 ssh.connect('192.168.1.20', port=22,username='admin', password='aabid')
