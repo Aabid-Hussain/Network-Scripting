533230418fca5e180b01a5fcf5edf7e3934b2399
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-08-21 08:39:49 -0700

    Started with pyshark package

---
+++ wireshark_package.py
@@ -0,0 +1,28 @@
+import pyshark
+import time
+
+'''
+capture = pyshark.FileCapture("E:\ME\Lab\RIP\Rip.pcapng")
+
+for inputline in capture:
+    print inputline
+    time.sleep(10)
+
+
+
+#capture = pyshark.LiveCapture(interface= 'Loopback')
+capture = pyshark.RemoteCapture('2.2.2.2',
+                 'Loopback',remote_port= 23)
+capture.sniff(timeout=50)
+
+for packet in capture.sniff_continuously(
+        packet_count= 5):
+    print("Just arrived: {}".format(packet))
+
+'''
+
+capture = pyshark.FileCapture("E:\ME\Lab\RIP\BGP_routes.pcapng")
+
+for inline in capture:
+    print (inline)
+    time.sleep(10)
