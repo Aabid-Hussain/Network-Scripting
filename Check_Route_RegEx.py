76995ca2124c6f12b54888f5d6d30d5ee818627e
Mohammad Aabid Hussain <er.aabidhussain@outlook.com>
2017-08-21 08:37:32 -0700

    worked on Route RegEx.py

--- Check_Route_RegEx.py
+++ Check_Route_RegEx.py
@@ -2,7 +2,7 @@
 
 import re
 
-Hash_line = "*#"*50
+Hash_line = "*#"*5
 
 show_output = """RTB# show ip route
 Codes: C - connected, S - static, R - RIP, M - mobile, B - BGP
@@ -24,11 +24,16 @@ O IA 151.108.1.0 [110/84] via 2.2.2.1, 00:12:11, Ethernet0/0
 O 131.108.2.0 [110/74] via 2.2.2.2, 00:12:23, Ethernet0/0
 O IA 131.108.1.0 [110/84] via 2.2.2.2, 00:12:11, Ethernet0/0"""
 
-def Search_OSPF_Route(Type_Of_Route, Network_Address, Gateway_Address):
+def Search_OSPF_Route():#Type_Of_Route, Network_Address, Gateway_Address):
     global show_output
     net,gateway =[],[]
-    regex = r"^%s.*\s([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+).*([0-9]+\.[0-9]+\.[0-9]+\.[0-9])" %Type_Of_Route
+    Type_Of_Route = 'O'
+    regex = r"^%s\s([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)" \
+            ".*([0-9]+\.[0-9]+\.[0-9]+\.[0-9])" \
+            %Type_Of_Route
+
     for lines in show_output.split('\n'):
+        #print(lines)
         matches = re.search(regex,lines)
         if matches:
             net.append(matches.group(1))
@@ -40,12 +45,13 @@ def Search_OSPF_Route(Type_Of_Route, Network_Address, Gateway_Address):
  #           print "%s Type of Route with network %s and gateway %s is not found" %(Type_Of_Route,Network_Address, Gateway_Address)
 
 print (Hash_line)
+#
+# Route_Type = raw_input("Enter the Type of Route: ")
+# Network = raw_input("Enter the Network address: ")
+# Gateway = raw_input("Enter the Gateway address:")
 
-Route_Type = raw_input("Enter the Type of Route: ")
-Network = raw_input("Enter the Network address: ")
-Gateway = raw_input("Enter the Gateway address:")
-
-Search_OSPF_Route(Route_Type,Network,Gateway)
+#Search_OSPF_Route(Route_Type,Network,Gateway)
+Search_OSPF_Route()
 
 
 
